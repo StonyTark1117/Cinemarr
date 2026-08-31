f7e5492c1b483839e8e9b222f0dc52dc9bd9ca8f
StonyTark1117 <braydonsanders2001@gmail.com>
2026-08-31 04:06:19 -0700

    Use remappable Minecraft 26 screenshot adapters

---
+++ platforms/mc26.2/common/src/main/java/stonytark/cinemarr/client/CinemarrVideoUiCapture.java
@@ -0,0 +1,16 @@
+package stonytark.cinemarr.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.Screenshot;
+import stonytark.cinemarr.Cinemarr;
+
+/** Minecraft 26.2 release-UI screenshot bridge. */
+final class CinemarrVideoUiCapture {
+    static void capture(Minecraft minecraft) {
+        Screenshot.grab(minecraft.gameDirectory, "cinemarr-video-ui-acceptance.png",
+                minecraft.gameRenderer.mainRenderTarget(), 1,
+                message -> Cinemarr.LOGGER.info("Acceptance video UI screenshot: {}", message.getString()));
+    }
+
+    private CinemarrVideoUiCapture() {}
+}
